7aeb3fb28c7af6833f7fdf6633465a48efd309e4
GabrielMeneze <65770195+GabrielMeneze@users.noreply.github.com>
2020-09-24 15:03:51 -0300

    Curso Repostory

---
+++ Repositories/CursoRepository.cs
@@ -0,0 +1,20 @@
+﻿using APIEdux.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIEdux.Repositories
+{
+    public class CursoRepository : ICurso
+    {
+        private readonly CursoRepository _cursoRepository;
+
+        public CursoRepository()
+        {
+            _cursoRepository = new CursoRepository();
+        }
+
+        
+    }
+}
