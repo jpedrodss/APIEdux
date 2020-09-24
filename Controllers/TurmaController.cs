2b7033ce5a2b4ecebefa408a5f054e98c349ee06
João Pedro <jpedrodss@gmail.com>
2020-09-24 14:00:02 -0300

    Turma feita

---
+++ Controllers/TurmaController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIEdux.Contexts;
+using APIEdux.Domains;
+using Microsoft.AspNetCore.Authorization;
+
+namespace APIEdux.Controllers
+{
+    [Authorize(Roles = "Professor")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurmaController : ControllerBase
+    {
+        private EduxContext _context = new EduxContext();
+
+        // GET: api/Turma
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Turma>>> GetTurma()
+        {
+            return await _context.Turma.ToListAsync();
+        }
+
+        // GET: api/Turma/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Turma>> GetTurma(int id)
+        {
+            var turma = await _context.Turma.FindAsync(id);
+
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return turma;
+        }
+
+        // PUT: api/Turma/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTurma(int id, Turma turma)
+        {
+            if (id != turma.IdTurma)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(turma).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TurmaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Turma
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Turma>> PostTurma(Turma turma)
+        {
+            _context.Turma.Add(turma);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTurma", new { id = turma.IdTurma }, turma);
+        }
+
+        // DELETE: api/Turma/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Turma>> DeleteTurma(int id)
+        {
+            var turma = await _context.Turma.FindAsync(id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            _context.Turma.Remove(turma);
+            await _context.SaveChangesAsync();
+
+            return turma;
+        }
+
+        private bool TurmaExists(int id)
+        {
+            return _context.Turma.Any(e => e.IdTurma == id);
+        }
+    }
+}
--- Repositories/TurmaRepository.cs
+++ Repositories/TurmaRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using APIEdux.Contexts;
+using APIEdux.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,112 @@ namespace APIEdux.Repositories
 {
     public class TurmaRepository
     {
+        private readonly EduxContext _ctx;
+
+        public TurmaRepository()
+        {
+            _ctx = new EduxContext();
+        }
+
+        /// <summary>
+        /// Adiciona uma Turma
+        /// </summary>
+        /// <param name="turma">Turma para ser adicionada</param>
+        public void Adicionar(Turma turma)
+        {
+            try
+            {
+                _ctx.Turma.Add(turma);
+
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Busca uma Turma por ID
+        /// </summary>
+        /// <param name="id">ID para pesquisa</param>
+        /// <returns>Turma pesquisada</returns>
+        public Turma BuscarID(int id)
+        {
+            try
+            {
+                return _ctx.Turma.Find(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message); ;
+            }
+        }
+
+        /// <summary>
+        /// Edita uma Turma
+        /// </summary>
+        /// <param name="turma">Turma a ser editada</param>
+        public void Editar(Turma turma)
+        {
+            try
+            {
+                Turma turmaTemp = BuscarID(turma.IdTurma);
+
+                if (turmaTemp == null)
+                    throw new Exception("Turma não encontrada.");
+
+                turmaTemp.Descricao = turma.Descricao;
+                turmaTemp.IdCurso = turma.IdCurso;
+                _ctx.Turma.Update(turmaTemp);
+
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message); ;
+            }
+        }
+
+        /// <summary>
+        /// Exclui uma Turma
+        /// </summary>
+        /// <param name="id">ID da Turma</param>
+        public void Excluir(int id)
+        {
+            try
+            {
+                Turma turma = BuscarID(id);
+
+                if (turma == null)
+                    throw new Exception("Tipo de Perfil não encontrado.");
+
+                _ctx.Turma.Remove(turma);
+
+                _ctx.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message); ;
+            }
+        }
+
+        /// <summary>
+        /// Lista todas as turmas
+        /// </summary>
+        /// <returns>Lista das turmas </returns>
+        public List<Turma> Listar()
+        {
+            try
+            {
+                List<Turma> turmas = _ctx.Turma.ToList();
+                return turmas;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message); ;
+            }
+        }
     }
 }
