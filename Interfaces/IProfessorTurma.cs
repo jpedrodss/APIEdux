dcdb306efa0d91fd13621d4c678166981d6eee61
arthuralvesoficial <arthurmarinho90@gmail.com>
2020-09-28 18:31:52 -0300

    Professor turma finazliado.
    Professor turma finazliado.

---
+++ Interfaces/IProfessorTurma.cs
@@ -0,0 +1,20 @@
+﻿using APIEdux.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIEdux.Interfaces
+{
+    interface IProfessorTurma
+    {
+        
+            void Adicionar(ProfessorTurma professorTurma);
+            void Excluir(int id);
+            void Editar(ProfessorTurma professorTurma);
+            List<ProfessorTurma> Listar();
+            ProfessorTurma BuscarID(int id);
+        }
+    
+}
+
